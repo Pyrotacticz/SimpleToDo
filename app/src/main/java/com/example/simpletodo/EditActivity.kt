564b6e115c31e90e6f40649b55fcb3e67cedb39c
jatruong <jatruong@uw.edu>
2022-01-18 19:56:59 -0800

    Fixed to allow text wrap on edit screen.

--- app/src/main/java/com/example/simpletodo/EditActivity.kt
+++ app/src/main/java/com/example/simpletodo/EditActivity.kt
@@ -4,6 +4,7 @@ package com.example.simpletodo
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
 import android.view.inputmethod.EditorInfo
 import android.widget.EditText
 
@@ -16,11 +17,13 @@ class EditActivity : AppCompatActivity() {
         val editText = findViewById<EditText>(R.id.editItem)
         editText.setText(getTextToEdit)
         editText.requestFocus()
+        editText.imeOptions = EditorInfo.IME_ACTION_DONE;
+        editText.setRawInputType(InputType.TYPE_CLASS_TEXT);
 
         editText.setOnEditorActionListener { textView, i, keyEvent ->
             if (i == EditorInfo.IME_ACTION_DONE) {
                 val data = Intent()
-                data.putExtra("text", editText.text.toString())
+                data.putExtra("text", editText.text.toString().trim().replace("\n", " "))
                 data.putExtra("position", intent.getStringExtra("position"))
                 setResult(RESULT_OK, data)
                 finish()
